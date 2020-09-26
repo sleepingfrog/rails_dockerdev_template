6ff8e6859af075792278a083eaad019a5586acc6
sleepingfrog <57852015+sleepingfrog@users.noreply.github.com>
2020-09-26 23:44:12 +0900

    parentheses

--- dockerdev.rb
+++ dockerdev.rb
@@ -10,67 +10,60 @@ run('bundle install')
 commit_all.call('bundle install')
 
 # Change README
-remove_file 'README.md'
-
-create_file 'README.md', <<~README
+remove_file('README.md')
+create_file('README.md', <<~README)
   # Dockerdev
   Imitating Martian technology.
   [Terraforming Rails](https://github.com/evilmartians/terraforming-rails)
   [dockerdev](https://github.com/evilmartians/terraforming-rails/tree/master/examples/dockerdev)
-  
+
   ## Provision
   ```sh
   dip provision
   ```
 README
-
-git add: 'README.md'
-git commit: '-m "README"'
+commit_all.call('readme')
 
 # use postgres
-comment_lines 'Gemfile', /gem 'sqlite3/
-
-gem 'pg', '>= 0.18', '< 2.0'
-
-gsub_file 'config/database.yml', /^default:.*?\n{2}/m, <<~YML
+comment_lines('Gemfile', /gem 'sqlite3/)
+gem('pg', '>= 0.18', '< 2.0')
+run('bundle install')
+gsub_file('config/database.yml', /^default:.*?\n{2}/m, <<~YML)
   default: &default
     adapter: postgresql
     encoding: unicode
     pool: <%= ENV.fetch("RAILS_MAX_THREADS") { 5 } %>
     url: <%= ENV['DATABASE_URL'] %>
-  
+
 YML
-gsub_file 'config/database.yml', /^development:.*?\n{2}/m, <<~YML
+gsub_file('config/database.yml', /^development:.*?\n{2}/m, <<~YML)
   development:
     <<: *default
     database: #{app_name}_development
-  
+
 YML
-gsub_file 'config/database.yml', /^test:.*?\n{2}/m, <<~YML
+gsub_file('config/database.yml', /^test:.*?\n{2}/m, <<~YML)
   test:
     <<: *default
     database: #{app_name}_test
-  
+
 YML
-gsub_file 'config/database.yml', /^production:.*?\z/m, <<~YML
+gsub_file('config/database.yml', /^production:.*?\z/m, <<~YML)
   production:
     <<: *default
     database: #{app_name}_production
     username: #{app_name}
     password: <%= ENV['#{app_name.upcase}_DATABASE_PASSWORD'] %>
 YML
-
-git add: '.'
-git commit: '-m "pg"'
+commit_all.call('postgres')
 
 # add rubocop
-
-get 'https://raw.githubusercontent.com/sleepingfrog/rails_dockerdev_template/master/.rubocop.yml', '.rubocop.yml'
+get('https://raw.githubusercontent.com/sleepingfrog/rails_dockerdev_template/master/.rubocop.yml', '.rubocop.yml')
 gem_group :development do
   gem 'rubocop'
 end
-git add: '.'
-git commit: '-m "rubocop"'
+run('bundle install')
+commit_all.call('rubocop')
 
 # add active_record_log task
 rakefile 'ar_log.rake' do
@@ -80,14 +73,12 @@ rakefile 'ar_log.rake' do
     end
   RUBY
 end
+commit_all.call('ar_log task')
 
-git add: '.'
-git commit: '-m "ar_log"'
 # Add docker-compose
-
-create_file 'docker-compose.yml', <<~DC
+create_file('docker-compose.yml', <<~DC)
   version: '3.4'
-  
+
   x-app: &app
     build:
       context: .
@@ -104,7 +95,7 @@ create_file 'docker-compose.yml', <<~DC
     image: #{app_name}_sample
     tmpfs:
       - /tmp
-  
+
   x-backend: &backend
     <<: *app
     stdin_open: true
@@ -125,7 +116,7 @@ create_file 'docker-compose.yml', <<~DC
       EDITOR: vi
     depends_on:
       - postgres
-  
+
   services:
     runner:
       <<: *backend
@@ -133,13 +124,13 @@ create_file 'docker-compose.yml', <<~DC
       ports:
         - '3000:3000'
         - '3002:3002'
-  
+
     rails:
       <<: *backend
       command: bundle exec rails server -b 0.0.0.0
       ports:
         - '3000:3000'
-  
+
     postgres:
       image: postgres:12
       volumes:
@@ -153,7 +144,7 @@ create_file 'docker-compose.yml', <<~DC
       healthcheck:
         test: pg_isready -U postgres -h 127.0.0.1
         interval: 5s
-  
+
     webpacker:
       <<: *backend
       command: bundle exec ./bin/webpack-dev-server
@@ -167,7 +158,7 @@ create_file 'docker-compose.yml', <<~DC
       environment:
         <<: *env
         WEBPACKER_DEV_SERVER_HOST: 0.0.0.0
-  
+
     redis:
       image: redis:6.0
       volumes:
@@ -188,15 +179,15 @@ create_file 'docker-compose.yml', <<~DC
     redis:
 DC
 
-create_file '.dockerdev/app/Dockerfile', <<~DOCKER
+create_file('.dockerdev/app/Dockerfile', <<~DOCKER)
   ARG RUBY_VERSION
   FROM ruby:$RUBY_VERSION-slim-buster
-  
+
   ARG PG_MAJOR
   ARG NODE_MAJOR
   ARG BUNDLER_VERSION
   ARG YARN_VERSION
-  
+
   # Common dependencies
   RUN apt-get update -qq \
     && DEBIAN_FRONTEND=noninteractive apt-get install -yq --no-install-recommends \
@@ -209,18 +200,18 @@ create_file '.dockerdev/app/Dockerfile', <<~DOCKER
     && rm -rf /var/cache/apt/archives/* \
     && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* \
     && truncate -s 0 /var/log/*log
-  
+
   # Add PostgreSQL to sources list
   RUN curl -sSL https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add - \
     && echo 'deb http://apt.postgresql.org/pub/repos/apt/ buster-pgdg main' $PG_MAJOR > /etc/apt/sources.list.d/pgdg.list
-  
+
   # Add NodeJS to sources list
   RUN curl -sL https://deb.nodesource.com/setup_$NODE_MAJOR.x | bash -
-  
+
   # Add Yarn to the sources list
   RUN curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - \
     && echo 'deb http://dl.yarnpkg.com/debian/ stable main' > /etc/apt/sources.list.d/yarn.list
-  
+
   # Install dependencies
   COPY .dockerdev/app/Aptfile /tmp/Aptfile
   RUN apt-get update -qq && DEBIAN_FRONTEND=noninteractive apt-get -yq dist-upgrade && \
@@ -233,7 +224,7 @@ create_file '.dockerdev/app/Dockerfile', <<~DOCKER
       apt-get clean && \
       rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* && \
       truncate -s 0 /var/log/*log
-  
+
   # Configure bundler
   ENV LANG=C.UTF-8 \
     BUNDLE_JOBS=4 \
@@ -242,54 +233,49 @@ create_file '.dockerdev/app/Dockerfile', <<~DOCKER
       rm /usr/local/lib/ruby/gems/*/specifications/default/bundler-*.gemspec && \
       gem uninstall bundler && \
       gem install bundler -v $BUNDLER_VERSION
-  
+
   RUN mkdir -p /app
-  
+
   WORKDIR /app
 DOCKER
 
-create_file '.dockerdev/app/Aptfile', <<~APT
+create_file('.dockerdev/app/Aptfile', <<~APT)
   vim
 APT
 
-create_file 'dip.yml', <<~DIP
+create_file('dip.yml', <<~DIP)
   version: '4.1'
-  
+
   environment:
     RAILS_ENV: development
-  
+
   compose:
     files:
       - docker-compose.yml
-    # specify project name explicitly to avoid name collisions:
-    # docker-compose use the compose file's folder name as the project name by default,
-    # which could be the same for different projects (e.g., if you store docker-compose.yml
-    # in the .dockerdev/ folder)
-    # project_name: my_project_dev
-  
+
   interaction:
     sh:
       description: Open a Bash shell within a Rails container (with dependencies up)
       service: runner
       command: /bin/bash
-  
+
     bash:
       description: Run an arbitrary script within a container (or open a shell without deps)
       service: runner
       command: /bin/bash
       compose_run_options: [no-deps]
-  
+
     bundle:
       description: Run Bundler commands
       service: runner
       command: bundle
       compose_run_options: [no-deps]
-  
+
     rake:
       description: Run Rake commands
       service: runner
       command: bundle exec rake
-  
+
     rails:
       description: Run Rails commands
       service: runner
@@ -300,36 +286,36 @@ create_file 'dip.yml', <<~DIP
           service: rails
           compose:
             run_options: [service-ports, use-aliases]
-  
+
     yarn:
       description: Run Yarn commands
       service: runner
       command: yarn
       compose_run_options: [no-deps]
-  
+
     test:
       description: Run Rails tests
       service: runner
       environment:
         RAILS_ENV: test
       command: bundle exec rails test
-  
+
     rubocop:
       description: Run Rubocop
       service: runner
       command: bundle exec rubocop
       compose_run_options: [no-deps]
-  
+
     psql:
       description: Run psql console
       service: postgres
       command: psql -h postgres -U postgres -d #{app_name}_development
-  
+
     'redis-cli':
       description: Run Redis console
       service: redis
       command: redis-cli -h redis
-  
+
   provision:
     - dip compose down --volumes
     - dip compose up -d postgres redis
@@ -337,22 +323,18 @@ create_file 'dip.yml', <<~DIP
     - dip yarn install
     - dip rake ar_log db:setup
 DIP
-
-git add: '.'
-git commit: '-m "dockerdev"'
+commit_all.call('dockerdev')
 
 # generator setting
-environment <<~RUBY
+environment(<<~RUBY)
   config.generators do |g|
     g.assets false
     g.halper false
     g.test_framework false
   end
 RUBY
-git add: '.'
-git commit: '-m "generator setting"'
+commit_all.call('generators')
 
 after_bundle do
-  git add: '.'
-  git commit: '-m "after bundle"'
+  commit_all.call('after bundle')
 end
